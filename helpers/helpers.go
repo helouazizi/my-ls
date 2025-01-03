cbd95b4624275b241e22a2061ed417da4f40301a
ouazizi <158299018+helouazizi@users.noreply.github.com>
2025-01-03 19:42:52 +0100

    check flag function

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -4,40 +4,86 @@ package helpers
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
-func Ls(flag, folderPath string) {
+func Scan() ([]string, []string, error) {
+	foldersPath := []string{}
+	flags := []string{}
+	input := os.Args
+	// lets extract folderpath and the plags if exist
+	for i := 1; i < len(input); i++ {
+		index := input[i]
+		if strings.HasPrefix(index, "-") {
+			exist, err := checkFlag(flags, index)
+			if err != nil {
+				return nil, nil, err
+			}
+			if exist {
+				continue
+			}
+			flags = append(flags, index)
+		} else {
+			foldersPath = append(foldersPath, index)
+		}
+	}
+	return flags, foldersPath, nil
+
+}
+func checkFlag(falgs []string, flag string) (bool, error) {
+	i := 0
+	// check if the flag allready exist
+	// if exist no need to do anything it not ann error
+
+	for _, oldFlag := range falgs {
+		if flag == oldFlag {
+			return true, nil
+		}
+	}
+	// check if the flag is valid
+	// the flag must have at most 2 "--"
+	for _, v := range flag {
+		if i > 1 {
+			return false, fmt.Errorf("ls: unrecognized option '%s'\nTry 'ls --help' for more information. ", flag)
+		}
+		if v == '-' {
+			i++
+		}
+		/*if i == 2 && len(flag) > 3 {
+			return false, fmt.Errorf("ls: unrecognized option '%s'\nTry 'ls --help' for more information. ", flag)
+		}*/
+
+	}
+	return false, nil
+}
+func ExtractContent(folderPath string) ([]os.FileInfo, error) {
+	_, err := os.Stat(folderPath)
+	if err != nil {
+		return nil, fmt.Errorf("ls: cannot access '%s': No such file or directory", folderPath)
+	}
 	dir, err := os.Open(folderPath)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 	defer dir.Close()
 	content, err := dir.Readdir(-1)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
-	fmt.Println("total", len(folderPath))
-	switch flag {
-	case "-l":
-		L(content, true)
-	case "-a":
-		L(content, false)
-	}
-
+	return content, nil
+}
+func PrintContent(flags []string, content []os.FileInfo) {
+	
 }
 
-func L(content []os.FileInfo, condition bool) {
-	for _, fi := range content {
-		if condition {
-			if strings.HasPrefix(fi.Name(), ".") || strings.HasPrefix(fi.Name(), "..") {
-				continue
-			}
+func Ls(flags, foldersPath []string) {
+	for _, folderPath := range foldersPath {
+		content, err := ExtractContent(folderPath)
+		if err != nil {
+			fmt.Println(err)
+			continue
 		}
-
-		fmt.Printf("%s %s %s %s %s\n", fi.Mode().String(), fi.Name(), strconv.Itoa(int(fi.Size())), fi.ModTime(), fi.ModTime().String())
+		PrintContent(flags, content)
 	}
+
 }
--- main.go
+++ main.go
@@ -4,25 +4,14 @@ package main
 import (
 	"fmt"
 	"myls/helpers"
-	"os"
 )
 
 func main() {
-	// lets check the arguments
-	if len(os.Args) > 3 {
-		fmt.Println("Usage: my-ls <option> <folder-name>")
+	falgs, foldersPath, err := helpers.Scan()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-
-	folderPath := "./" // default folder
-	option := ""
-	if len(os.Args) == 3 {
-		option = os.Args[1]
-		folderPath = os.Args[2]
-	} else if len(os.Args) == 2 {
-		option = os.Args[1]
-		folderPath = "./" // default folder
-	}
-	helpers.Ls(option, folderPath)
+	helpers.Ls(falgs, foldersPath)
 
 }
